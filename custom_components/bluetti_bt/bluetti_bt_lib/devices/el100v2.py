184a74f19aefda6b53d4771b2440aa6ced658479
etcy <96890476+e7chiu@users.noreply.github.com>
2025-09-15 16:10:47 -0400

    Update and rename elite100v2.py to el100v2.py

--- custom_components/bluetti_bt/bluetti_bt_lib/devices/el100v2.py
+++ custom_components/bluetti_bt/bluetti_bt_lib/devices/el100v2.py
@@ -3,6 +3,6 @@
 from ..base_devices.ProtocolV2Device import ProtocolV2Device
 
 
-class Elite100V2(ProtocolV2Device):
+class EL100V2(ProtocolV2Device):
     def __init__(self, address: str, sn: str):
-        super().__init__(address, "Elite100V2", sn)
+        super().__init__(address, "EL100V2", sn)
